fcade62810980167de1baa813e70520d45b799c2
Almond <93007463+EldariaDevelopment@users.noreply.github.com>
2023-01-02 21:08:00 -0800

    Update Experimental.js

--- Experimental.js
+++ Experimental.js
@@ -1,3 +1,4 @@
+// If Functions
 function TellDay(date) {
     var date1 = new Date().toLocaleDateString();
     const diffInMs = new Date(date1) - new Date(date);
@@ -8,3 +9,16 @@ function TellDay(date) {
     }
 }
 TellDay("05/16/2022");
+// Switch functions
+function TellDay(date) {
+    var date1 = new Date().toLocaleDateString();
+    const diffInMs = new Date(date1) - new Date(date);
+    switch(diffInMs > 0) {
+    	case true:
+			return(["Days since "+date +": "+Math.floor(diffInMs/86400000)]);
+      	case false:
+			return(["Days until "+date+": "+Math.abs(Math.floor(diffInMs/86400000))]);
+    }
+}
+
+alert(TellDay("1/1/2021"));
