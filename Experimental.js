0490fe6d547e38edeb2a29ec18e397dd404050f0
Almond <93007463+EldariaDevelopment@users.noreply.github.com>
2023-01-02 21:11:04 -0800

    Update Experimental.js

--- Experimental.js
+++ Experimental.js
@@ -1,23 +1,26 @@
 // If Functions
+
 function TellDay(date) {
-    var date1 = new Date().toLocaleDateString();
-    const diffInMs = new Date(date1) - new Date(date);
-    if (diffInMs > 0) {
-        alert("Days since", date + ":",(Math.floor(diffInMs/(86400000)))-1);
-    } else {
-        alert("Days Until", date + ":",(Math.abs(Math.floor(diffInMs/(86400000)))+1))
-    }
+	var date1=new Date().toLocaleDateString();
+	const diffInMs=new Date(date1)-new Date(date);
+	if (diffInMs>0) {
+		alert("Days since", date + ":",(Math.floor(diffInMs/(86400000)))-1);
+	} else {
+		alert("Days Until", date + ":",(Math.abs(Math.floor(diffInMs/(86400000)))+1))
+	}
 }
 TellDay("05/16/2022");
+
 // Switch functions
+
 function TellDay(date) {
-    var date1 = new Date().toLocaleDateString();
-    const diffInMs = new Date(date1) - new Date(date);
-    switch(diffInMs > 0) {
+    var date1=new Date().toLocaleDateString();
+    const diffInMs=new Date(date1)-new Date(date);
+    switch(diffInMs>0) {
     	case true:
-			return(["Days since "+date +": "+Math.floor(diffInMs/86400000)]);
+		return(["Days since "+date +": "+Math.floor(diffInMs/86400000)]);
       	case false:
-			return(["Days until "+date+": "+Math.abs(Math.floor(diffInMs/86400000))]);
+		return(["Days until "+date+": "+Math.abs(Math.floor(diffInMs/86400000))]);
     }
 }
 
